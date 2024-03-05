f9643f47af884de8637d8f835912c0ef2bf5756e
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-03-05 11:37:23 +0100

    Simplified launch script

--- runBAPS.sh
+++ runBAPS.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Runs BAPS on terminal mode
-matlab -nosplash -nodesktop -r "run('add_BAPS_to_path.m'); run('general/baps6_terminal.m');"
+MATLAB_SCRIPTS="run('add_BAPS_to_path.m'); run('general/baps6_terminal.m');"
+matlab -nosplash -nodesktop -r "$MATLAB_SCRIPTS"
