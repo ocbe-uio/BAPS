08f9ad5afe46ad636fe52589394ddd4a8576c851
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-03-08 15:26:38 +0100

    Unwrapped calculation of `popLogml` (#6)

--- greedyMix/computePopulationLogml.m
+++ greedyMix/computePopulationLogml.m
@@ -4,12 +4,14 @@ function popLogml = computePopulationLogml(pops, adjprior, priorTerm)
 
   global COUNTS;
   global SUMCOUNTS;
-  x = size(COUNTS,1);
-  y = size(COUNTS,2);
+  x = size(COUNTS, 1);
+  y = size(COUNTS, 2);
   z = length(pops);
 
-  popLogml = ...
-  squeeze(sum(sum(reshape(...
-  gammaln(repmat(adjprior,[1 1 length(pops)]) + COUNTS(:,:,pops)) ...
-  ,[x y z]),1),2)) - sum(gammaln(1+SUMCOUNTS(pops,:)),2) - priorTerm;
+  rep_adj = repmat(adjprior, [1 1 z]);
+  gamma_rep_counts = gammaln(rep_adj + COUNTS(:, :, pops));
+  gamma_sum_counts = sum(gammaln(1 + SUMCOUNTS(pops, :)), 2);
+  gamma_rep_counts_sum = sum(sum(reshape(gamma_rep_counts, [x y z]), 1), 2);
+  gamma_rep_counts_reshaped = squeeze(gamma_rep_counts_sum);
+  popLogml = gamma_rep_counts_reshaped - gamma_sum_counts - priorTerm;
 end
