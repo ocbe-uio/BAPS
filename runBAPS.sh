4e7d98fc19b7decebd01fabc4c027fa4884467e2
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-06-25 11:39:51 +0200

    `runBAPS.sh` bypasses `baps.m` now

--- runBAPS.sh
+++ runBAPS.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Runs BAPS on terminal mode
-MATLAB_SCRIPTS="run('general/add_BAPS_to_path.m'); run('baps.m');"
-matlab -nosplash -nodesktop -r "$MATLAB_SCRIPTS"
+MATLAB_STARTUP_SCRIPT="run('general/add_BAPS_to_path.m'); run('general/welcome.m');"
+MATLAB_PATH="/usr/local/MATLAB/R2023b/bin/matlab"
+"$MATLAB_PATH" -nosplash -nodesktop -r "$MATLAB_STARTUP_SCRIPT"
