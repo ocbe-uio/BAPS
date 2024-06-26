ocbe-uio/BAPS

commit e7396a07879a02e243c6c8c29f75698018dcb599

Automatic whitespace removal

greedyMix/greedyMix.m
@@ -29,10 +29,6 @@ function greedyMix(tietue, file_type, partitionCompare)
   Z = tietue.Z;
   clear tietue;
 
-  global PARTITION; global COUNTS;
-  global SUMCOUNTS; global POP_LOGML;
-  clearGlobalVars;
-
   c.data=data;
   c.noalle = noalle; c.adjprior = adjprior; c.priorTerm = priorTerm;
   c.dist=dist; c.Z=Z; c.rowsFromInd = rowsFromInd;

baps.m
@@ -27,7 +27,7 @@ function baps(file, file_type, analysis, partitionCompare)
   end
 
   % Processing boolean input
-  partitionCompare = process_boolean_input(4);
+  partitionCompare = process_boolean_input(4); % i.e., false (0) if missing
 
   % Dispatching analysis
   switch analysis

graph/linkage.m
@@ -1,5 +1,5 @@
 function Z = linkage(Y, method)
-%LINKAGE Create hierarchical cluster tree. 
+%LINKAGE Create hierarchical cluster tree.
 %   Z = LINKAGE(Y) creates a hierarchical cluster tree, using the single
 %   linkage algorithm.  The input Y is a distance matrix such as is
 %   generated by PDIST.  Y may also be a more general dissimilarity
@@ -16,7 +16,7 @@ function Z = linkage(Y, method)
 %      'ward'     --- inner squared distance
 %
 %   Cluster information will be returned in the matrix Z with size m-1
-%   by 3, where m is the number of observations in the original data. 
+%   by 3, where m is the number of observations in the original data.
 %   Column 1 and 2 of Z contain cluster indices linked in pairs
 %   to form a binary tree. The leaf nodes are numbered from 1 to
 %   m. They are the singleton clusters from which all higher clusters
@@ -43,7 +43,7 @@ function Z = linkage(Y, method)
 %   See also PDIST, INCONSISTENT, COPHENET, DENDROGRAM, CLUSTER, CLUSTERDATA,
 %   KMEANS, SILHOUETTE.
 
-%   Copyright 1993-2002 The MathWorks, Inc. 
+%   Copyright 1993-2002 The MathWorks, Inc.
 %   $Revision: 1.16 $
 
 [k, n] = size(Y);
@@ -51,10 +51,10 @@ function Z = linkage(Y, method)
 
 m = (1+sqrt(1+8*n))/2;
 if k ~= 1 | m ~= fix(m)
-  error('The first input has to match the output of the PDIST function in size.');   
+  error('The first input has to match the output of the PDIST function in size.');
 end
 
-if nargin == 1 % set default switch to be 'co' 
+if nargin == 1 % set default switch to be 'co'
    method = 'co';
 end
 
@@ -72,26 +72,26 @@ Z = zeros(m-1,3); % allocate the output matrix.
 
 N = zeros(1,2*m-1);
 N(1:m) = 1;
-n = m; % since m is changing, we need to save m in n. 
+n = m; % since m is changing, we need to save m in n.
 R = 1:n;
 
 for s = 1:(n-1)
    X = Y;
-        
+
    [v, k] = min(X);
-      
+
    i = floor(m+1/2-sqrt(m^2-m+1/4-2*(k-1)));
    j = k - (i-1)*(m-i/2)+i;
-   
+
    Z(s,:) = [R(i) R(j) v]; % update one more row to the output matrix A
-   
+
    % Update Y.  In order to vectorize the computation, we need to compute all
    % the indices corresponding to cluster i and j in Y, denoted by I and J.
    I1 = 1:(i-1); I2 = (i+1):(j-1); I3 = (j+1):m; % these are temp variables.
    U = [I1 I2 I3];
    I = [I1.*(m-(I1+1)/2)-m+i i*(m-(i+1)/2)-m+I2 i*(m-(i+1)/2)-m+I3];
    J = [I1.*(m-(I1+1)/2)-m+j I2.*(m-(I2+1)/2)-m+j j*(m-(j+1)/2)-m+I3];
-   
+
    switch method
    case 'si' %single linkage
       Y(I) = min(Y(I),Y(J));
@@ -108,10 +108,10 @@ for s = 1:(n-1)
    end
    J = [J i*(m-(i+1)/2)-m+j];
    Y(J) = []; % no need for the cluster information about j.
-   
+
    % update m, N, R
-   m = m-1; 
+   m = m-1;
    N(n+s) = N(R(i)) + N(R(j));
    R(i) = n+s;
-   R(j:(n-1))=R((j+1):n); 
+   R(j:(n-1))=R((j+1):n);
 end

graph/viewMixPartition.m
@@ -33,10 +33,10 @@ for i=1:nind
     % Suhteellisten osuuksien laskeminen
     pop = partition(i);
     pop = find(eiTyhjatPopulaatiot==pop);
-           
+
     % Pylv‰‰n piirt‰minen
     h0 =patch([i-1, i, i, i-1], [0, 0, 1, 1], varit(pop,:));
-    set(h0,'EdgeColor','none'); % Midevaa varten kommentoitava!   
+    set(h0,'EdgeColor','none'); % Midevaa varten kommentoitava!
 
 end
 
@@ -48,13 +48,13 @@ if ~isempty(popnames)
         firstInd = popnames{i,2};
         if size(popnames,1) ~=nind
             line([firstInd-1, firstInd-1], [0,1], 'Color', 'k');  %Populaatioiden rajat
-        end            
+        end
         if i<npops
             x_paikka = popnames{i,2}-1+(popnames{i+1,2}-popnames{i,2})/2;
         else
             x_paikka = popnames{i,2}-1+(nind+1-popnames{i,2})/2;
         end
-               
+
         korkeuskerroin = pieninarvo / -0.2;
         suhdekerroin = npops/6;
         for letter_num = 1:length(popnames{i,1}{1})
@@ -85,4 +85,4 @@ function extra = korjaus(letter)
         extra = -0.003;
     else
         extra = 0;
-end;
+end

greedyMix/indMix.m
@@ -1,4 +1,5 @@
 function [logml, npops, partitionSummary] = indMix(c, npops, dispText)
+  dispText = true;
   % Greedy search algorithm with unknown number of classes for regular
   % clustering.
   % Input npops is not used if called by greedyMix or greedyPopMix.
@@ -84,8 +85,7 @@ function [logml, npops, partitionSummary] = indMix(c, npops, dispText)
     ninds = size(rows,1);
 
     initialPartition = admixture_initialization(initData, npops, Z);
-    [sumcounts, counts, logml] = ...
-    initialCounts(initialPartition, data, npops, rows, noalle, adjprior);
+    [sumcounts, counts, logml] = initialCounts(initialPartition, data, npops, rows, noalle, adjprior);
     PARTITION = zeros(ninds, 1);
     for i=1:ninds
       apu = rows(i);
@@ -96,7 +96,6 @@ function [logml, npops, partitionSummary] = indMix(c, npops, dispText)
     SUMCOUNTS = sumcounts;
     POP_LOGML = computePopulationLogml(1:npops, adjprior, priorTerm);
     LOGDIFF = repmat(-Inf,ninds,npops);
-    clear initialPartition; clear counts; clear sumcounts;
 
     % PARHAAN MIXTURE-PARTITION ETSIMINEN
     nRoundTypes = 7;
@@ -131,8 +130,7 @@ function [logml, npops, partitionSummary] = indMix(c, npops, dispText)
           muutosNyt = 0;
           for ind = inds
             i1 = PARTITION(ind);
-            [muutokset, diffInCounts] = laskeMuutokset(ind, rows, ...
-            data, adjprior, priorTerm);
+            [muutokset, diffInCounts] = laskeMuutokset(ind, rows, data, adjprior, priorTerm);
 
             if round==1
               [maxMuutos, i2] = max(muutokset);

greedyMix/laskeMuutokset.m
@@ -9,8 +9,10 @@ function [muutokset, diffInCounts] = ...
   % Otettu kהyttצצn globaali muuttuja LOGDIFF, johon on tallennettu muutokset
   % logml:ssה siirrettהessה yksilצitה toisiin populaatioihin.
 
-  global COUNTS;      global SUMCOUNTS;
-  global PARTITION;   global POP_LOGML;
+  global COUNTS;
+  global SUMCOUNTS;
+  global PARTITION;
+  global POP_LOGML;
   global LOGDIFF;
 
   npops = size(COUNTS,3);
@@ -42,7 +44,6 @@ function [muutokset, diffInCounts] = ...
   COUNTS(:,:,i2) = COUNTS(:,:,i2)-repmat(diffInCounts, [1 1 ni2]);
   SUMCOUNTS(i2,:) = SUMCOUNTS(i2,:)-repmat(diffInSumCounts,[ni2 1]);
 
-  muutokset(i2) = new_i1_logml - i1_logml ...
-  + new_i2_logml - i2_logml;
+  muutokset(i2) = new_i1_logml - i1_logml + new_i2_logml - i2_logml;
   LOGDIFF(ind,:) = muutokset;
 end

commit a58b3d7aa78832e7ca52960197f04ce994613d1e

Fixed regression from e7396a0

greedyMix/greedyMix.m
@@ -29,6 +29,10 @@ function greedyMix(tietue, file_type, partitionCompare)
   Z = tietue.Z;
   clear tietue;
 
+  global PARTITION; global COUNTS;
+  global SUMCOUNTS; global POP_LOGML;
+  clearGlobalVars;
+
   c.data=data;
   c.noalle = noalle; c.adjprior = adjprior; c.priorTerm = priorTerm;
   c.dist=dist; c.Z=Z; c.rowsFromInd = rowsFromInd;