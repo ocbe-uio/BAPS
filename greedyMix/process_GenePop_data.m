581702a74480c7a59ca707060e233d1e8d88e6d3
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-08-26 13:15:51 +0200

    Fixed processing of BAPS and GenePop on greedyMix() (#9)

--- greedyMix/process_GenePop_data.m
+++ greedyMix/process_GenePop_data.m
@@ -11,16 +11,10 @@ function processed_data = process_GenePop_data(filename)
 
   [data, rowsFromInd, alleleCodes, noalle, adjprior, priorTerm] = handleData(data);
   [Z,dist] = newGetDistances(data,rowsFromInd);
-  save_preproc = questdlg('Do you wish to save pre-processed data?',...
-  'Save pre-processed data?',...
-  'Yes','No','Yes');
-  if isequal(save_preproc,'Yes');
-    waitALittle;
-    [filename, pathname] = uiputfile('*.mat','Save pre-processed data as');
-    kokonimi = [pathname filename];
-    c.data = data; c.rowsFromInd = rowsFromInd; c.alleleCodes = alleleCodes;
-    c.noalle = noalle; c.adjprior = adjprior; c.priorTerm = priorTerm;
-    c.dist = dist; c.popnames = popnames; c.Z = Z;
-    save(kokonimi,'c','-v7.3'); % added by Lu Cheng, 08.06.2012
-    processed_data = c;
-  end
+
+  % Forming and returning pre-processed data
+  c.data = data; c.rowsFromInd = rowsFromInd; c.alleleCodes = alleleCodes;
+  c.noalle = noalle; c.adjprior = adjprior; c.priorTerm = priorTerm;
+  c.dist = dist; c.popnames = popnames; c.Z = Z;
+  processed_data = c;
+end
