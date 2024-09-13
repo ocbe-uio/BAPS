562485f98ccf3243e8a2ff30c56bc024d6651be4
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-09-13 11:59:03 +0200

    Fixed path in version bump script. Bumped version

--- general/baps.m
+++ general/baps.m
@@ -3,7 +3,7 @@ function baps(file, file_type, analysis, coordinates, ~) %TODO: move to general?
   addpath(genpath(cd));
 
   % Welcome message
-  ver = [6 0 0 9003];
+  ver = [6 0 0 9004];
   versionStr = sprintf('%d.%d.%d.%d', ver(1), ver(2), ver(3), ver(4));
   disp(['Welcome to BAPS ' versionStr]);
 
