7a42ff842d9873282305395ed0aae9acd6e7a3cf
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-09-13 12:05:29 +0200

    Added GenePop demo for greedyMix

--- general/welcome.m
+++ general/welcome.m
@@ -1,2 +1,3 @@
 disp('Welcome to BAPS! For a demo of the greedyMix() function, run:')
 disp('baps("ExampleData/spatial clustering of individuals/Example data in BAPS format for clustering of haploid individuals.txt", "BAPS", "greedyMix")')
+disp('baps("ExampleData/spatial clustering of individuals/Example GenePop Data.txt", "GenePop", "greedyMix")')
