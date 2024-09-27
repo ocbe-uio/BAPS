ffe3786cadd08202eeb4a5aa4df473fda8877893
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-09-27 13:03:22 +0200

    Removed duplicated functions

--- greedyMix/greedyMix.m
+++ greedyMix/greedyMix.m
@@ -759,37 +759,6 @@ end
 
 
 
-function logml = ...
-  initialCounts(partition, data, npops, rows, noalle, adjprior)
-
-  nloci=size(data,2);
-  ninds = size(rows, 1);
-
-  koot = rows(:,1) - rows(:,2) + 1;
-  maxSize = max(koot);
-
-  counts = zeros(max(noalle),nloci,npops);
-  sumcounts = zeros(npops,nloci);
-  for i=1:npops
-    for j=1:nloci
-      havainnotLokuksessa = find(partition==i & data(:,j)>=0);
-      sumcounts(i,j) = length(havainnotLokuksessa);
-      for k=1:noalle(j)
-        alleleCode = k;
-        N_ijk = length(find(data(havainnotLokuksessa,j)==alleleCode));
-        counts(k,j,i) = N_ijk;
-      end
-    end
-  end
-
-  %initializeGammaln(ninds, maxSize, max(noalle));
-
-  logml = laskeLoggis(counts, sumcounts, adjprior);
-
-end
-%-----------------------------------------------------------------------
-
-
 function logml=computeLogml(counts, sumcounts, noalle, data, rowsFromInd)
   nloci = size(counts,2);
   npops = size(counts,3);
@@ -1198,15 +1167,4 @@ function [emptyPop, pops] = findEmptyPop(npops)
     popDiff = diff([0 pops npops+1]);
     emptyPop = min(find(popDiff > 1));
   end
-end
-
-%--------------------------------------------------------------------
-
-function loggis = laskeLoggis(counts, sumcounts, adjprior)
-  npops = size(counts,3);
-
-  logml2 = sum(sum(sum(gammaln(counts+repmat(adjprior,[1 1 npops]))))) ...
-  - npops*sum(sum(gammaln(adjprior))) - ...
-  sum(sum(gammaln(1+sumcounts)));
-  loggis = logml2;
 end
