1319d1db5383855f8cbf896709ef8827baac269e
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-08-21 10:55:02 +0200

    Future-proof MATLAB binary

--- general/runBAPS.sh
+++ general/runBAPS.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Runs BAPS on terminal mode
 MATLAB_STARTUP_SCRIPT="run('general/add_BAPS_to_path.m'); run('general/welcome.m');"
-MATLAB_PATH="/usr/local/MATLAB/R2023b/bin/matlab"
+MATLAB_PATH="/usr/local/bin/matlab"
 "$MATLAB_PATH" -nosplash -nodesktop -r "$MATLAB_STARTUP_SCRIPT"
